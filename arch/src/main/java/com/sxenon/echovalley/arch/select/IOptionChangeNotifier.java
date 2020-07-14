d950cf81183780df487860f9e3fcd27e5fdad344
jsui <jsui@qti.qualcomm.com>
2020-07-14 09:32:38 +0800

    Add some interface for Select Module

---
+++ arch/src/main/java/com/sxenon/echovalley/arch/select/IOptionChangeNotifier.java
@@ -0,0 +1,9 @@
+package com.sxenon.echovalley.arch.select;
+
+import java.util.List;
+
+public interface IOptionChangeNotifier<T> {
+    void onOptionRemoved(int position);
+    void onSelectedOptionsRemoved(List<Boolean> selectedFlags);
+    void notifySelectChange(List<Boolean> oldSelectedFlags, List<Boolean> newSelectedFlags);
+}
